0319c432ad9714e161c45411e4aebbb0d0445207
jerry <jerry.mo@fulaan.com>
2017-01-11 10:23:17 +0800

    HaHa

--- src/main/java/com/batis/app/dao/impl/UserDAOImpl.java
+++ src/main/java/com/batis/app/dao/impl/UserDAOImpl.java
@@ -2,19 +2,23 @@ package com.batis.app.dao.impl;
 
 import com.batis.app.dao.UserDAO;
 import com.batis.app.model.User;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by moslpc on 2017/1/11.
  */
 @Service
-public class UserDAOImpl extends JdbcDaoSupport implements UserDAO {
+public class UserDAOImpl implements UserDAO {
+
+    @Autowired
+    private JdbcOperations jdbcOperations;
 
     @Override
     public void save(User user) {
         String sql = "insert into user values(?,?,?)";
-        this.getJdbcTemplate().update(sql, user.getId(), user.getUserName(),
+        jdbcOperations.update(sql, user.getId(), user.getUserName(),
                user.getPassword());
     }
 }
